a93459e1026a75d84a56761031cd94ea05c62afd
satorioh <wangbinxp@gmail.com>
2018-06-11 17:44:16 +0800

    add pages

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <home-header></home-header>
+    <home-tabs></home-tabs>
+  </div>
+</template>
+
+<script>
+import HomeHeader from './components/Header'
+import HomeTabs from './components/Tabs'
+export default {
+  name: 'Home',
+  components: {
+    'home-header': HomeHeader,
+    'home-tabs': HomeTabs
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>header</div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
